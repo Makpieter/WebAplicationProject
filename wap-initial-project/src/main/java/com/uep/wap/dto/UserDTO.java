11f7805ed985dc6b2a55b564fc6cfc3b3741c7de
Makpieter <pietermakowski@gmail.com>
2026-03-18 15:49:58 +0100

    UserDTO_update/fix

--- wap-initial-project/src/main/java/com/uep/wap/dto/UserDTO.java
+++ wap-initial-project/src/main/java/com/uep/wap/dto/UserDTO.java
@@ -2,8 +2,16 @@ package com.uep.wap.dto;
 
 public class UserDTO {
 
-    private String username;
+    public String username;
     private String password;
+    public String email;
+    public Enum role;
+    public Integer reputation;
+    public String bio;
+    public Enum status;
+    public Date createdAt;
+    public Date updatedAt;
+
 
     public String getUsername() {
         return username;
@@ -19,6 +27,62 @@ public class UserDTO {
 
     public void setPassword(String name) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Enum getRole() {
+        return role;
+    }
+
+    public void setRole(Enum role) {
+        this.role = role;
+    }
+
+    public Integer getReputation() {
+        return reputation;
+    }
+
+    public void setReputation(Integer reputation) {
+        this.reputation = reputation;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public Enum getStatus() {
+        return status;
+    }
+
+    public void setStatus(Enum status) {
+        this.status = status;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
     }
 
 
